bc68b0d1c73631f4aadc956f94f13eabb10d09ae
guiltyshu <36754033+guiltyshu@users.noreply.github.com>
2020-06-17 23:27:56 +0700

    Create function show News

--- NewsWeb/NewsWeb/Controllers/HomeController.cs
+++ NewsWeb/NewsWeb/Controllers/HomeController.cs
@@ -3,28 +3,48 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using NewsWeb.Models;
+using PagedList;
 
 namespace NewsWeb.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        QLTintucDataContext data = new QLTintucDataContext();
+
+        private List<TINTUC> getNews(int count)
         {
-            return View();
+            return data.TINTUCs.OrderByDescending(a => a.Ngaycapnhat).Take(count).ToList();
+        }
+        
+        public ActionResult Index(int ? page)
+        {
+            int pageSizes = 5;
+            int pageNum = (page ?? 1);
+
+            var news = getNews(6);
+            return View(news.ToPagedList(pageNum, pageSizes));
         }
 
-        public ActionResult About()
+        public ActionResult Chudetintuc()
         {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
+            var chudetintuc = from cd in data.CHUDETINTUCs select cd;
+            return PartialView(chudetintuc);
         }
 
-        public ActionResult Contact()
+        public ActionResult TinTheochude(int id, int? page)
         {
-            ViewBag.Message = "Your contact page.";
+            int pageSizes = 5;
+            int pageNum = (page ?? 1);
 
-            return View();
+            var news = from n in data.TINTUCs where n.MaCD == id select n;
+            return View(news.ToPagedList(pageNum, pageSizes));
+        }
+
+        public ActionResult Details(int id)
+        {
+            var tintuc = from t in data.TINTUCs where t.Matintuc == id select t;
+            return View(tintuc.Single());
         }
     }
 }
